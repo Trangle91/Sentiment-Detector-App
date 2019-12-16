718818eaafcd21339d374aace43071d2882e9331
Trang Le <le009109@stthomas.edu>
2019-12-16 14:15:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Created on Sun Dec 15 03:27:57 2019
 
 from flask import Flask, request, jsonify, render_template
 import pickle
+from load.py import *
 import keras.models
 import tensorflow as tf
 from keras.models import model_from_json
@@ -14,22 +15,14 @@ from keras.preprocessing.text import Tokenizer
 app = Flask(__name__)
 
 global model, graph
-json_file = open('model.json','r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-#load woeights into new model
-model.load_weights("model_weights.h5")
-
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-
+model, graph = init()
 tokenizer = pickle.load(open('convert.pkl','rb'))
 
 @app.route('/',methods=[])
 def home():
     return render_template('index.html')
         
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
         message = request.form['message']
