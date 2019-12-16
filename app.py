bbef6de915c4932af4dfae9c71f307dc402a8030
Trang Le <le009109@stthomas.edu>
2019-12-16 13:40:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
         vect = tokenizer.transform(data).toarray()
         prediction = model.predict(vect)
   
-    return render_template('index.html', Result='The sentence is {}'.format(result))
+    return render_template('result.html', Result='The sentence is {}'.format(result))
 
 
 if __name__ == "__main__":
