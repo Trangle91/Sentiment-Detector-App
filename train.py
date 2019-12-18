45023677e2d1a96e2bf9608b66940de4112ee5e6
Trang Le <le009109@stthomas.edu>
2019-12-18 12:03:49 -0600

    Update train.py
    changed to LSTM

--- train.py
+++ train.py
@@ -1,135 +1,94 @@
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import RandomizedSearchCV
-
-from keras.models import Sequential
-from keras import layers
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.wrappers.scikit_learn import KerasClassifier
-
-import os
 
 import pandas as pd
+import numpy as np
+import tensorflow as tf
 
-filepath_dict = {'yelp':   './yelp_labelled.txt',
-                 'amazon': './amazon_cells_labelled.txt',
-                 'imdb':   './imdb_labelled.txt'}
+df = pd.read_csv("yelp_combined.txt", names=['sentence', 'label'], sep='\t')
 
-df_list = []
-for source, filepath in filepath_dict.items():
-    df = pd.read_csv(filepath, names=['sentence', 'label'], sep='\t')
-    df['source'] = source  # Add another column filled with the source name
-    df_list.append(df)
+#spliting data
+X = df['sentence']
+y = df['label']
+X[0], y[0]
 
-df = pd.concat(df_list)
+#cleaning text
+import re
+def clean_X(text):
+  text = text.split(".")
+  text = ' '.join(text)
+  text = text.lower()
+  text = re.sub("[^a-zA-Z\- ]"," ",text)
+  text = re.sub(" +"," ",text)
+  text = text.strip()
+  return text
 
+X = X.apply(lambda text: clean_X(text))
 
-def plot_history(history):
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    x = range(1, len(acc) + 1)
+# Creating vocab
+vocab = []
+for sen in X:
+  sen = sen.lower().split(" ")
+  vocab.extend(sen)
+vocab = list(set(vocab))
+vocab.append("<PAD>")
+vocab_size = len(vocab)
+print("Vocab :: ",vocab)
+print("Vocab size ::",vocab_size)
 
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(x, acc, 'b', label='Training acc')
-    plt.plot(x, val_acc, 'r', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-    plt.subplot(1, 2, 2)
-    plt.plot(x, loss, 'b', label='Training loss')
-    plt.plot(x, val_loss, 'r', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
+# Find sentence with maximum words and find the number of words present in it
+max_len_sentence = None 
+max_len = 0 
+for sen in X:
+  no_of_words = len(sen.split())
+  if no_of_words>max_len:
+    max_len = no_of_words
+    max_len_sentence = sen
+max_len_sentence, max_len
 
-plot_history(history)
+# <PAD>
+X = [X[idx].split() + ["<PAD>"] * (max_len - len(X[idx].split()) ) for idx in range(len(X))]
 
-def create_model(num_filters, kernel_size, vocab_size, embedding_dim, maxlen):
-    model = Sequential()
-    model.add(layers.Embedding(vocab_size, embedding_dim, input_length=maxlen))
-    model.add(layers.Conv1D(num_filters, kernel_size, activation='relu'))
-    model.add(layers.GlobalMaxPooling1D())
-    model.add(layers.Dense(10, activation='relu'))
-    model.add(layers.Dense(1, activation='sigmoid'))
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    return model
+# Converting all samples to indices
+word_to_id = {vocab[word_id]:word_id for word_id in range(len(vocab))}
+numerized_sentences = []
+for sen in X:
+  sen = [word_to_id[word] for word in sen]
+  numerized_sentences.append(sen)
 
-param_grid = dict(num_filters=[32, 64, 128],
-                  kernel_size=[3, 5, 7],
-                  vocab_size=[5000], 
-                  embedding_dim=[50],
-                  maxlen=[100])
+print("Numerized data :",numerized_sentences)
 
-epochs = 20
-embedding_dim = 50
-maxlen = 100
-output_file = 'output.txt'
+np.array(numerized_sentences).shape
 
-# Run grid search for each source (yelp, amazon, imdb)
-for source, frame in df.groupby('source'):
-    print('Running grid search for data set :', source)
-    sentences = df['sentence'].values
-    y = df['label'].values
+dimension = 50
+embedding_matrix = np.random.rand(vocab_size, dimension)
+embedding_matrix
 
-    # Train-test split
-    sentences_train, sentences_test, y_train, y_test = train_test_split(
-        sentences, y, test_size=0.25, random_state=1000)
+#creating vector matrices 
+tf.reset_default_graph()
+tf_embedding_matrix = tf.Variable(embedding_matrix)
 
-    # Tokenize words
-    tokenizer = Tokenizer(num_words=5000)
-    tokenizer.fit_on_texts(sentences_train)
-    X_train = tokenizer.texts_to_sequences(sentences_train)
-    X_test = tokenizer.texts_to_sequences(sentences_test)
+sen_indices = tf.placeholder(dtype=tf.int32, shape=[None,None])
+sen_to_vec = tf.nn.embedding_lookup(tf_embedding_matrix, sen_indices)
+rnn_cell = tf.compat.v1.nn.rnn_cell.BasicRNNCell(num_units=16)
+sen_to_vec = tf.cast(sen_to_vec, tf.float32)
+outputs_for_tagging, output_for_classification = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=sen_to_vec, dtype=tf.float32)
 
-    # Adding 1 because of reserved 0 index
-    vocab_size = len(tokenizer.word_index) + 1
-    # Pad sequences with zeros
-    X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
-    X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+classification_logits = tf.squeeze(tf.contrib.layers.fully_connected(output_for_classification, 1, activation_fn=tf.sigmoid))
+classification_targets = tf.placeholder(dtype=tf.float32 ,shape=[None])
+classification_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=classification_targets, logits=classification_logits))
+train_op = tf.train.AdamOptimizer(0.01).minimize(classification_loss)
 
-    # Parameter grid for grid search
-    param_grid = dict(num_filters=[32, 64, 128],
-                      kernel_size=[3, 5, 7],
-                      vocab_size=[vocab_size],
-                      embedding_dim=[embedding_dim],
-                      maxlen=[maxlen])
-    model_out = KerasClassifier(build_fn=create_model,
-                            epochs=epochs, batch_size=10,
-                            verbose=False)
-    grid = RandomizedSearchCV(estimator=model_out, param_distributions=param_grid,
-                              cv=4, verbose=1, n_iter=5)
-    grid_result = grid.fit(X_train, y_train)
+# """check accuracy"""
+# correct_prediction = tf.equal(tf.argmax(classification_logits,1), tf.argmax(classification_targets,1))
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    # Evaluate testing set
-    test_accuracy = grid.score(X_test, y_test)
-
-    s = ('Running {} data set\nBest Accuracy : '
-             '{:.4f}\n{}\nTest Accuracy : {:.4f}\n\n')
-    output_string = s.format(
-        source,
-        grid_result.best_score_,
-        grid_result.best_params_,
-        test_accuracy)
-    print(output_string)
-
-# saving model
-json_model = model.model.to_json()
-open('model.json', 'w').write(json_model)
-# saving weights
-model.model.save_weights('model_weights.h5', overwrite=True)
-
-#saving dataset
-df.to_csv(r'text_data.csv')
-
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  for epoch in range(50):
+    feed_dict = {sen_indices: numerized_sentences, classification_targets: y}
+    output_classification, cl_logits, loss, _ = sess.run([classification_targets,
+                                                                        classification_logits,
+                                                                        classification_loss,
+                                                                        train_op], feed_dict=feed_dict)
+    print("Rnn final outputs:: ", output_classification)
+    print("Loss :: ",loss)
+    # print("Accuracy :: ",acc)
