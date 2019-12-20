a856009fff01eb0de64d7866c3a0212c13c6181e
Trang Le <le009109@stthomas.edu>
2019-12-19 23:27:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,3 @@
-# """
-# Created on Sun Dec 15 03:27:57 2019
-
-# @author: jadele
-# """
-
-# from flask import Flask, request, jsonify, render_template
-# import pickle
-# from load import init
-# import keras.models
-
-# from keras.models import model_from_json
-# from keras.preprocessing.text import Tokenizer
-
-# #loading Flask
-# app = Flask(__name__)
-
-# #loading model
-# global model, graph
-# model, graph = init()
-# #loading tokenizer
-# cv = pickle.load(open('transform.pkl','rb'))
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-        
-# @app.route('/predict',methods=['GET','POST'])
-# def predict():
-#     if request.method == 'POST':
-#         message = request.form['message']
-#         data = [message]
-#         vect = cv.transform(data).toarray()
-
-#         prediction = model.predict(vect)
-#         if prediction == 1:
-#             result = "positive"
-#         else:
-#             result = "negative"
-        
-#     return render_template('index.html', Result='The sentiment is {}'.format(result))
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 import pickle
 import keras.models
 from flask import Flask, request, jsonify, render_template
@@ -59,11 +12,11 @@ from keras.models import model_from_json
 #Loading Flask
 app = Flask(__name__)
 
-# Model reconstruction from JSON file
+#Model reconstruction from JSON file
 with open('model.json', 'r') as f:
     model = model_from_json(f.read())
 
-# Loading weights into the new model
+#Loading weights into the new model
 model.load_weights('model_weights.h5')
 
 #Loading tokenizer
