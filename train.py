dc5a9919b190c207bd230ed4b6d4a135e7b69790
Trang Le <le009109@stthomas.edu>
2020-01-13 10:33:26 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd 
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
